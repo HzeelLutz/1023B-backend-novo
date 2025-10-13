9b1ccf15a98ea8491b5b28e52e0e9b165eac9ee6
Lorena Mascarenhas <lorena.santos@estudante.ifms.edu.br>
2025-10-13 11:50:26 -0400

    Fazendo login 13.10

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -1,23 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'dotenv/config'
-import rotasAutenticadas from './rotas/rotas-autenticadas.js'
 import rotasNaoAutenticadas from './rotas/rotas-nao-autenticadas.js'
+import rotasAutenticadas from './rotas/rotas-autenticadas.js'
 import Auth from './middlewares/auth.js'
+import cors from 'cors'
 const app = express()
-//Esse middleware faz com que o 
-// express faça o parse do body da requisição para json 
+app.use(cors())
 
 app.use(express.json())
 
 
 // Usando as rotas definidas em rotas.ts
-// Usando o middleware de autenticação
 app.use(rotasNaoAutenticadas)
 app.use(Auth)
 app.use(rotasAutenticadas)
-
-//Não usando o middleware de autenticação
-app.use(rotasNaoAutenticadas)
 
 // Criando o servidor na porta 8000 com o express
 app.listen(8000, () => {
